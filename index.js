e4b22ad94068e956e927ce4ee1d11e3b76a93710
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-03-16 15:47:42 -0400

    Get started on the provisioning for a droplet.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 /* jshint node: true */
 'use strict';
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+var path = require('path');
+var DigitalOceanDeployPlugin = require('./lib/digitalocean-deploy-plugin');
+var ProvisionCommand = require('./lib/commands/provision');
 
 module.exports = {
-  name: 'ember-cli-deploy-digitalocean'
+  name: 'ember-cli-deploy-digitalocean',
+
+  includedCommands: function() {
+    return { 'do:provision': ProvisionCommand };
+  },
+
+  createDeployPlugin: function(options) {
+    return new DigitalOceanDeployPlugin({
+      name: options.name
+    });  
+  }
 };
